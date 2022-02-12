fab5b142927eb6cbc55e7f61b64311d867093f4b
JR Blanco <jr.cosio1@gmail.com>
2022-02-12 13:19:12 +0100

    Comienzo 2 Evaluacion

---
+++ Programación/Tareas/PROG06_Tarea/src/Concesionario/Concesionario.java
@@ -0,0 +1,152 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Concesionario;
+
+import Tarea5.Dni;
+import Tarea5.Vehiculo;
+import java.time.LocalDate;
+
+
+/**
+ *
+ * @author José Ramón Blanco Gutiérrez
+ */
+public class Concesionario {
+    private Vehiculo[] vehiculos;               //Array de los vehiculos.
+    private int numVehiculos;                   //Número de vehiculos en el concesionario
+    
+    private final int MAXIMO_VEHICULOS = 50;    //Constate con el número maximo de vehiculos en el consesionario
+
+    /**
+     * CONTRUCTOR
+     */
+    public Concesionario() {
+        this.vehiculos = new Vehiculo[MAXIMO_VEHICULOS];    //Crea el array con el maximo de 50 (0-49)
+        this.numVehiculos = 0;                              //Inicializa a cero el número de vehiculos
+    }
+    
+    /**
+     * Método para buscar un vehiculo por su matricula
+     * 
+     * @param matricula Matricula del vehiculo que se desa buscar
+     * @return Un cadena con los datos del vehiculo encontrado o null en caso de error
+     */
+    public String buscaVehiculo(String matricula){
+        for (int i=0;i<this.numVehiculos;i++) {
+            if (vehiculos[i].getMatricula().equalsIgnoreCase(matricula)) {
+                return "{Marca: " + vehiculos[i].getMarca() +           //Al encontrar el vehiculo llamamos a return que finaliza el método terminando el bucle.
+                       " Matricula: " + vehiculos[i].getMatricula() +
+                       " Precio: " + vehiculos[i].getPrecio() + "€}";
+            }
+        }
+        return null;    //Si ha recorrido todo el array y no hay ninguna coincidencia termina el bucle y retorna null.
+    }
+    
+    /**
+     * Imprime en pantalla la lista de todos los vehiculos del concesionario
+     * 
+     */
+    public void listaVehiculos() {
+        if (this.numVehiculos != 0) {                                               //Solo si tiene vehiculos
+            System.out.println("-----------------------------------------------------------------------------");
+            System.out.printf("%20s | %10s | %6s | %7s | %s\n","Marca","Matricula","Precio","Km","Descripción");
+            System.out.println("-----------------------------------------------------------------------------");
+            for (int i=0;i<this.numVehiculos;i++) {                                 //Recorre el array hasta el numero de vehiculos en el concesionario               
+                System.out.printf("%20s | %10s | %6.2f | %7d | %s\n",vehiculos[i].getMarca(),
+                                                                     vehiculos[i].getMatricula(),
+                                                                     vehiculos[i].getPrecio(),
+                                                                     vehiculos[i].getKilometros(),
+                                                                     vehiculos[i].getDescripcion());
+            }
+            System.out.println("-----------------------------------------------------------------------------");
+        } else {
+            System.out.println("No hay vehiculos almacenados\n");                   //Muestra que no hay nada
+        }
+    }
+
+    /**
+     *  Método que Inserta un nuevo vehiculo en el array de Vehiculos[]
+     * 
+     * @param marca Marca del vehiculo
+     * @param matricula Matricula del vehiculo nuevo
+     * @param kilometros Km del vehiculo
+     * @param fecha Fecha
+     * @param descripcion Descripción
+     * @param precio Precio
+     * @param nombre Nombre del propietario 
+     * @param dni DNI del propietario
+     * 
+     * @return 0 Correcto, -1 Array lleno, -2 Existe ese vehiculo
+     */
+    public int insertaVehiculo(String marca, String matricula, int kilometros, LocalDate fecha, String descripcion, float precio, String nombre, Dni dni) {
+        
+        int valorRetorno;                                   //Uso esta variable para guardar los estados de retorno y solo usar un return
+        
+        if (this.numVehiculos < MAXIMO_VEHICULOS) {         //Si tenemos nemos vehiculo que el máximo 
+            if (buscaVehiculo(matricula) == null) {         //Si es null es que no existe
+                vehiculos[numVehiculos] = new Vehiculo(marca, matricula, kilometros, fecha, descripcion, precio, nombre, dni); //En la posición que indica creamos el objeto
+                numVehiculos++;                             // Incrementamos 1 el número de vehiculos
+                valorRetorno = 0;                           //Correcto
+            } else {
+                valorRetorno = -2;                          // Existe esa vehiculo
+            }
+        } else {
+            valorRetorno = -1;                              // Esta lleno el concesionario
+        }
+        return valorRetorno;
+     
+    }
+    
+    /**
+     * Actualiza los kilometros de un vehiculo
+     * @param matricula Matricula del vehiculo que se desea cambiar
+     * @param nuevosKms Nuevos Kilometros
+     * @return 
+     */
+    public boolean actualizaKms(String matricula, int nuevosKms) {
+        
+        for (int i=0;i<this.numVehiculos;i++) {
+            if (vehiculos[i].getMatricula().equalsIgnoreCase(matricula)) {
+                 vehiculos[i].setKilometros(nuevosKms);
+                 return true;
+            }
+        }
+        return false;
+    }
+    
+    /**
+     * Método que borra un vehiculo del array
+     * 
+     * @param matricula Matricula del vehiculo que queremos borrar
+     * @return 0 todo correcto, -1 no hay vehiculos que borrar, -2 Vehiculo no extiste
+     */
+    public int borrarVehiculo(String matricula) {
+        Vehiculo[] aux = new Vehiculo[MAXIMO_VEHICULOS];
+        
+        int codRetorno;         //Donde se guardan los codigos para retornar al final del método
+        int contAux = 0;        //Contador para el indice de aux
+        
+        if (this.numVehiculos != 0) {
+            if (this.buscaVehiculo(matricula)!=null) {
+                for (int i = 0; i<this.numVehiculos; i++){ 
+                    if (!this.vehiculos[i].getMatricula().equalsIgnoreCase(matricula)) {
+                        aux[contAux] = this.vehiculos[i];   //Guarda el vehiculo que sea distinto al buscado
+                        contAux++;                          //Aumenta solo cuando se guarda un vehiculo
+                    } 
+                }
+                this.vehiculos = aux;               //Sustitulle el Array Vehiculos por el nuevo creado sin el que se desea borrar
+                this.numVehiculos--;                //Restamos 1 los vehiculos del concesionario.
+                codRetorno = 0;
+            } else {
+                codRetorno = -2;                    //El vehiculo no existe;
+            }
+        } else {
+            codRetorno = -1;                        // No hay vehiculos para borrar
+        }
+        
+        return codRetorno;
+    }
+    
+}
---
+++ Programación/Tareas/PROG06_Tarea/src/Tarea5/Dni.java
@@ -0,0 +1,118 @@
+
+package Tarea5;
+
+/**
+ * Clave de DNI, para almacenar y validar el DNI/NIF
+ * 
+ * @author José Ramón Blanco
+ */
+public class Dni {
+     private int numDni;  
+    
+    /**
+     * Metodo Getter que retorno el número del DNI
+     * @return Numero entero del DNI
+     */
+    public int getDni() {
+        return numDni;
+    }
+
+    /**
+     * Método para retornar el NIF
+     * @return Retorna el numero y la letra correspondiente;
+     */
+    public String getNif() {
+        return (""+this.numDni+Dni.calcularLetraNIF(numDni));
+    }
+       
+    /**
+     * Método para establecer el DNI
+     * 
+     * @param numNIF Número de DNI junto con su letra 00000000T
+     * @throws Exception Lanza una excepción si el numero de NIF no es correcto.
+     */
+    public void setDni(String numNIF) throws Exception {
+        
+        if (Dni.validarNIF(numNIF)) {
+            this.numDni = Dni.extraerNumeroNIF(numNIF);
+        } else {
+            throw new Exception ("NIF Invalido: " + numNIF);
+        }
+        
+    }
+      
+    /**
+     * Método para establever el DNI en base al número
+     * 
+     * @param dni Solo el número de DNI son la letra
+     * @throws Exception Lanza una excepción si el número de DNI no es válido
+     */
+    public void setDni(int dni) throws Exception {
+        
+        if (dni>999999 && dni<99999999) {
+           this.numDni = dni; 
+        } else {
+            throw new Exception ("DNI invalido: "+ String.valueOf(dni));
+        }
+        
+    }   
+    
+    /**
+     * Calcula la letra del NIF
+     * 
+     * @param dni el numero de DNI
+     * @return retorna la letra del NIF
+     */
+    public static char calcularLetraNIF(int dni){
+        String secuenciaLetrasNIF = "TRWAGMYFPDXBNJZSQVHLCKE";
+        return secuenciaLetrasNIF.charAt(dni % 23);
+    }
+    
+    /**
+     * Valida si el número de NIF es correcto, es decir esta bien formado
+     * 
+     * @param nif El número de DNI y su correcpondiente letra
+     * @return Retorna true si es Válido.
+     */
+    public static boolean validarNIF(String nif) {
+        int numero;
+        char letra;
+        boolean valido = false;
+        
+        
+        if (nif== null) {
+            valido = false;
+        } else if (nif.length()<8 || nif.length()>9) {
+            valido = false;
+        } else {
+            numero = Dni.extraerNumeroNIF(nif);
+            letra = Dni.extraerLetraNIF(nif);
+
+            if (letra == Dni.calcularLetraNIF(numero)) {
+                valido = true;
+            }
+        }
+        return valido;
+    }
+    
+    /**
+     * Extrae la letra de un NIF
+     * 
+     * @param nif Número de DNI y su letra
+     * @return Retorna la letra del NIF
+     */
+    public static char extraerLetraNIF(String nif) {
+        return Character.toUpperCase(nif.charAt(nif.length()-1)); 
+    }
+    
+    /**
+     * Extrae de un NIF el número de DNI
+     * 
+     * @param nif Numero del DNI y su Letra
+     * @return Retorna el numero que compone el NIF
+     */
+    public static int extraerNumeroNIF(String nif) {
+        return Integer.parseInt(nif.substring(0, nif.length()-1));
+    }
+    
+}
